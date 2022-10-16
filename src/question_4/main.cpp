8e9d61d170dec5c6762270651cc693a6d8722110
antmar00 <anthony.martinezcastro@g.austincc.edu>
2022-10-16 03:10:45 +0000

    midterm

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,37 @@
+#include "question4.h"
+using std::cout, std::cin;
 
 int main()
 {
+    std::vector <int> prime_nums;
+    int num;
 
+    while(true)
+    {
+        cout << "\nPrime Numbers\n";
+        cout << "\nEnter a number between 1 and 60, else press 0 to exit: ";        
+        cin >> num;
+
+        if (num == 0)
+        {
+            cout << "Until next time.";
+            break;
+        }
+
+        else if (num < 1 || num > 60)
+        {
+            cout<<"Not in between 1 and 60...\n";
+        }
+        else if (num > 1 || num < 60)
+        {
+            prime_nums = get_primes(num);
+            cout << "The prime numbers for " << num << " are\n";
+            for (auto num: prime_nums)
+            {
+                cout << num;
+            }         
+        }   
+    }
     return 0;
+
 }
